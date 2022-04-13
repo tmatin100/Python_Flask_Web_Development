04e79ff715fa899c36890acf862d353c5feffd6c
tmatin-at-038599014972 <tmatin100@gmail.com>
2022-04-13 15:53:27 -0400

    first commit

---
+++ application.py
@@ -0,0 +1,19 @@
+#import flask      https://flask.palletsprojects.com/en/2.1.x/
+from flask import Flask
+
+#create a new app by createing a variable traditinally called app and invoke the Flask class constructor 
+app = Flask(__name__)
+
+#create a route
+@app.route('/')
+def index():                  #create a function for this route 
+    return 'hello'
+
+#set envrionment variables on your python vertiual environnment flask application
+# export FLASK_APP=application.py 
+# export FLASK_ENV=development 
+# flask run 
+
+@app.route('/api/testimonials')
+def testimonials():
+    return {'testimonial': ['great', 'its ok', 'fantastic','G-Unit']}
