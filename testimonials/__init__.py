1936aa28564b3b9daca6afd27455d6c3b95dd578
tmatin-at-038599014972 <tmatin100@gmail.com>
2022-04-14 15:38:59 -0400

    larger application

---
+++ testimonials/__init__.py
@@ -0,0 +1,9 @@
+#import flask      https://flask.palletsprojects.com/en/2.1.x/patterns/packages/
+from flask import Flask
+
+#create a new app by createing a variable traditinally called app and invoke the Flask class constructor 
+app = Flask(__name__)
+
+
+#import the routes from the route file 
+import testimonials.routes 
---
+++ testimonials/route.py
@@ -0,0 +1,11 @@
+# we neet to import app for the routes to use it 
+from testimonials import app 
+
+@app.route('/')
+def index():                  #create a function for this route 
+    return 'hello'
+
+
+@app.route('/api/testimonials')
+def testimonials():
+    return {'testimonial': ['great', 'its ok', 'fantastic',]}
