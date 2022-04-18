e8bd7ccef811469f1a27ecf798fac3e69e3ac233
tmatin-at-038599014972 <tmatin100@gmail.com>
2022-04-18 12:07:16 -0400

    updated html template

--- testimonials/route.py
+++ testimonials/route.py
@@ -1,9 +1,11 @@
 # we neet to import app for the routes to use it 
 from testimonials import app 
+from flask import render_template
 
 @app.route('/')
 def index():                  #create a function for this route 
-    return '<h1> hello world</h1>'
+    #return '<h1> hello world</h1>'
+    return render_template('index.html')
 
 
 @app.route('/api/testimonials')
