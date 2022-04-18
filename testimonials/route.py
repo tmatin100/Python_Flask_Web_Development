0e7cf6d55a8d91b6f1890463afa1497466b6d715
tmatin-at-038599014972 <tmatin100@gmail.com>
2022-04-18 12:53:07 -0400

    update

--- testimonials/route.py
+++ testimonials/route.py
@@ -1,18 +1,34 @@
 # we neet to import app for the routes to use it 
-from re import T
 from testimonials import app 
 from flask import render_template
 
-test = [ 
-    {'id': 10, 
-    'name': 'Tamzidul', 
-    'message': 'Your courses helped me land a new job! '}
+testimonials = [ 
+    {
+        'id': 10, 
+        'name': 'Tamzidul', 
+        'message': 'Your courses helped me land a new job! '
+    },
+
+    { 
+        'id': 35, 
+        'name': 'Aakash', 
+        'message': 'Never have I understood OOP until now'
+        
+    },
+
+    { 
+        'id': 40, 
+        'name': 'Maitn', 
+        'message': 'I watched all 200 hrs staright!'
+        
+    }
+
 ]
 
 @app.route('/')
 def index():                  #create a function for this route 
     #return '<h1> hello world</h1>'
-    return render_template('index.html', testimonials = test)
+    return render_template('index.html', testimonials = testimonials)
 
 
 #@app.route('/api/testimonials')
