e482d210f4eece9e8bf1e89846e664eabe86bf29
tmatin-at-038599014972 <tmatin100@gmail.com>
2022-04-18 14:46:11 -0400

    added 404 and changed routes

--- testimonials/routes.py
+++ testimonials/routes.py
@@ -1,6 +1,6 @@
 # we neet to import app for the routes to use it 
 from testimonials import app 
-from flask import render_template
+from flask import render_template, abort 
 
 testimonials = [ 
     { 
@@ -27,16 +27,27 @@ testimonials = [
 ]
 
 @app.route('/')
-def index():                  #create a function for this route 
+@app.route('/testimonials')
+def show_testimonial():                  #create a function for this route 
     #return '<h1> hello world</h1>'
     return render_template('index.html', testimonials = testimonials)
 
-@app.route('/<id>')
+@app.route('/testimonials/<id>')
 def show_testimonial(id): 
 
     for testimonial in testimonials:
         if testimonial.get('id') == int(id):
             return render_template('testimonial.html', testimonial = testimonial)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+abort(404)
+
+
+@app.route('/testimonials')
+
 
 #@app.route('/api/testimonials')
 #def testimonials():
